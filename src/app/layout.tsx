b92da1fd08f21dba25123c76bdb3f89099a40cdb
BuildTools <hohoaisan@gmail.com>
2024-02-22 21:51:14 +0700

    chore: update metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
+import { Metadata } from 'next';
 import React from 'react';
 
 import '@/styles/globals.css';
 
 import { ThemeProvider } from '@/contexts/theme';
+
+export const metadata: Metadata = {
+  title: 'Live2D Playground',
+  description:
+    'A hobby Live2D viewer for the web, including basic feature: live2d zip file viewer, motion and facial tracking and more yet to come!',
+};
 
 export default async function RootLayout({
   children,
